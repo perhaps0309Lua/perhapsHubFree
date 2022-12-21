ef2f8e24d8e57c4493b2bbb4fbe6911c3c6ef1f1
perhapsmaybe <68890108+perhapsmaybe@users.noreply.github.com>
2022-12-21 15:04:04 -0700

    Move Addons into Folder and add Example game

---
+++ Games/Busy Business.lua
@@ -0,0 +1,5 @@
+local Repository = 'https://raw.githubusercontent.com/perhaps0309Lua/perhapsHubFree/main/'
+
+local Library = loadstring(game:HttpGet(Repository..'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(Repository..'Addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(Repository..'Addons/SaveManager.lua'))()
---
+++ Loader.lua
@@ -0,0 +1,5 @@
+local Repository = 'https://raw.githubusercontent.com/perhaps0309Lua/perhapsHubFree/main/Games/'
+
+if game.GameId == 234234234 then 
+    loadstring(game:HttpGet(Repository..'Busy Business.lua'))()
+end 
